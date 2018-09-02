45f6c994853e3c65a99e873f18bcb1008da9b1a8
Ilgiz Absalyamov <aik.fiend@gmail.com>
2018-09-03 01:16:05 +0300

    Trying to fix configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
 import 'hammerjs';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,9 @@ import 'hammerjs';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
     MatToolbarModule,
     FlexLayoutModule
   ],
